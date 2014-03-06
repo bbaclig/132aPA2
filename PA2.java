dd39a9bbb28bf630c1ed014c4fbd45bbb54da3de
Nhu-quynh N Liu <n1liu@ieng6-202.ucsd.edu>
2014-03-05 23:32:43 -0800

    Everything

--- PA2.java
+++ PA2.java
@@ -1,3 +1,9 @@
+/**
+ * Group members:
+ * (1) 
+ * (2) Nhu-Quynh Liu, A10937319
+ */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,7 +18,7 @@ public class PA2 {
     try {
 
       Class.forName("org.sqlite.JDBC");
-      conn = DriverManager.getConnection("jdbc:sqlite:pa2-2.db");
+      conn = DriverManager.getConnection("jdbc:sqlite:pa2.db");
       System.out.println("Opened database successfully");
 
       Statement stmt = conn.createStatement();
@@ -20,33 +26,62 @@ public class PA2 {
       stmt.executeUpdate("DROP TABLE IF EXISTS QuartersToGraduation;");
       stmt.executeUpdate("CREATE TABLE QuartersToGraduation( Student char(32), Quarters int DEFAULT 0);");
       stmt.executeUpdate("INSERT INTO QuartersToGraduation (Student) " +
-                         "SELECT Student " +
+                         "SELECT DISTINCT Student " +
                          "FROM Record;");
-      
-      String coreQuery = "SELECT DISTINCT r1.Student, c.Course " +
-                         "FROM Core c, Record r1 " +
+
+/*
+      stmt.executeUpdate("CREATE TABLE AllCourses( Course char(32) );");
+      stmt.executeUpdate("INSERT INTO AllCourse( Course ) " +
+                         "SELECT * FROM Core UNION SELECT * FROM Elective " +
+                         "FROM Record;");*/
+
+      String coreQuery = "INSERT INTO Record (Student, Course) "  +
+                         "SELECT DISTINCT r1.Student, c.Course " +
+                         "FROM ( SELECT * FROM Core UNION ALL SELECT * FROM Elective ) c, Record r1 " +
                          "WHERE NOT EXISTS ( " +
-                         "SELECT * " +
+                         "SELECT Course " +
                          "FROM Prerequisite AS p " +
                          "WHERE p.course = c.Course " +
                          "AND NOT EXISTS( " +
-                         "SELECT * " +
+                         "SELECT Course " +
                          "FROM Record AS r2 " +
                          "WHERE r2.student = r1.student AND p.prereq = r2.course)) " +
                          "AND NOT EXISTS ( " +
-                         "SELECT * FROM Record AS r3 " +
+                         "SELECT Course FROM Record AS r3 " +
                          "WHERE r1.Student = r3.Student " +
                          "AND c.course = r3.course)";
+
       int delta = 0;
 
       do {
         /*
         stmt.executeUpdate("UPDATE QuartersToGraduation " +
                            "SET Quarters = Quarters + 1 " +
-                           "WHERE Student IN (" + coreQuery + ");");*/
+                           "WHERE Student IN ( SELECT Student FROM (" + coreQuery + "));");*/
+
+        stmt.executeUpdate("DELETE FROM Record " +
+                           "WHERE Student IN ( " +
+                           "SELECT Student FROM Record r1 " +
+                           "WHERE NOT EXISTS( " +
+                           "SELECT Course FROM Core " +
+                           "WHERE NOT EXISTS( " +
+                           "SELECT Course FROM Record r2 " +
+                           "WHERE r2.Course = Core.course AND r2.Student = r1.Student)))" +
+                           "AND Student IN ( " + 
+                           "SELECT Student FROM (Elective LEFT JOIN Record) c " +
+                           "GROUP BY Student " +
+                           "HAVING COUNT(c.Course) >= 5);");                          
+
+        delta = stmt.executeUpdate("UPDATE QuartersToGraduation " +
+                           "SET Quarters = Quarters + 1 " +
+                           "WHERE Student IN ( SELECT Student FROM Record);");
         
+        stmt.executeUpdate( coreQuery );
+
+        /*
         delta = stmt.executeUpdate("INSERT INTO Record (Student, Course) " +
                                                                     coreQuery);
+        */
 
         System.out.println( delta );
       } while( delta != 0 );
