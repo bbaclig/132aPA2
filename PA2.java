ccad232d72850a9ccd6699f613afdf27a3a53c18
Nhu-quynh N Liu <n1liu@ieng6-202.ucsd.edu>
2014-03-05 20:05:25 -0800

    Changed .java

--- PA2.java
+++ PA2.java
@@ -1,72 +1,66 @@
-/** 
- * Group members: 
- * (1) Benigno Baclig, A10108582 
- * (2) Mimi Liu, A
- */ 
-import java.sql.Connection; 
-import java.sql.DriverManager; 
-import java.sql.SQLException; 
-import java.sql.Statement; 
-import java.sql.PreparedStatement; 
-import java.sql.ResultSet; 
- 
-public class PA2 { 
- 
-	public static void main(String[] args) { 
- 
-		Connection conn = null; 
- 
-		try { 
-			// Load JDBC class 
-			Class.forName("org.sqlite.JDBC"); 
-			// Get the connection to the database 
-			conn = DriverManager.getConnection("jdbc:sqlite:pa2.db"); 
-			System.out.println("Opened database successfully"); 
- 
-			// Case #1: Modification 
-			Statement stmt = conn.createStatement(); 
-			stmt.executeUpdate("DROP TABLE IF EXISTS DBpre;"); 
-			stmt.executeUpdate("CREATE TABLE DBpre (course VARCHAR(20));"); 
-			stmt.executeUpdate("INSERT INTO DBpre VALUES ('Java Prog');"); 
- 
-			// Case #2: Query the Prerequisite table 
-			ResultSet rset= stmt.executeQuery("SELECT * from Prerequisite;"); 
-			// Print the Course and Prereq columns 
-			System.out.println ("\nStatement result:"); 
-			while (rset.next()) { 
-				System.out.print(rset.getString("Course")); 
-				System.out.print("---"); 
-				System.out.println(rset.getString("Prereq"));  
-			} 
- 
-			// Case #3: Prepared statements 
-			PreparedStatement pstmt= conn.prepareStatement("SELECT * FROM "
-					+ "Prerequisite WHERE Course = ? OR Prereq = ?;"); 
-			pstmt.setString (1, "Database"); 
-			pstmt.setString (2, "Database"); 
-			rset= pstmt.executeQuery (); 
-			System.out.println ("\nPrepared statement result:"); 
-			while (rset.next()) { 
-				System.out.print(rset.getString("Course")); 
-				System.out.print("---"); 
-				System.out.println(rset.getString("Prereq")); 
-			} 
- 
-			// close the result set, statement 
-			rset.close(); 
-			stmt.close(); 
-		} catch (Exception e) { 
-			throw new RuntimeException("There was a problem!", e); 
-		} finally { 
-			// I have to close the connection in the "finally" clause otherwise 
-			// in case of an exception i would leave it open. 
-			try { 
-				if (conn != null) 
-					conn.close(); 
-			} catch (SQLException e) { 
-				throw new RuntimeException("Help! I could not close the "
-						+ "connection!!!", e); 
-			} 
-		} 
-	}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PA2 {
+  public static void main( String[] args ) {
+    Connection conn = null;
+    
+    try {
+
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection("jdbc:sqlite:pa2-2.db");
+      System.out.println("Opened database successfully");
+
+      Statement stmt = conn.createStatement();
+
+      stmt.executeUpdate("DROP TABLE IF EXISTS QuartersToGraduation;");
+      stmt.executeUpdate("CREATE TABLE QuartersToGraduation( Student char(32), Quarters int DEFAULT 0);");
+      stmt.executeUpdate("INSERT INTO QuartersToGraduation (Student) " +
+                         "SELECT Student " +
+                         "FROM Record;");
+      
+      String coreQuery = "SELECT DISTINCT r1.Student, c.Course " +
+                         "FROM Core c, Record r1 " +
+                         "WHERE NOT EXISTS ( " +
+                         "SELECT * " +
+                         "FROM Prerequisite AS p " +
+                         "WHERE p.course = c.Course " +
+                         "AND NOT EXISTS( " +
+                         "SELECT * " +
+                         "FROM Record AS r2 " +
+                         "WHERE r2.student = r1.student AND p.prereq = r2.course)) " +
+                         "AND NOT EXISTS ( " +
+                         "SELECT * FROM Record AS r3 " +
+                         "WHERE r1.Student = r3.Student " +
+                         "AND c.course = r3.course)";
+      int delta = 0;
+
+      do {
+        /*
+        stmt.executeUpdate("UPDATE QuartersToGraduation " +
+                           "SET Quarters = Quarters + 1 " +
+                           "WHERE Student IN (" + coreQuery + ");");*/
+        
+        delta = stmt.executeUpdate("INSERT INTO Record (Student, Course) " +
+                                                                    coreQuery);
+
+        System.out.println( delta );
+      } while( delta != 0 );
+          stmt.close();
+    } catch ( Exception e ) {
+      throw new RuntimeException("There was a problem!", e);
+    } finally {
+      try{
+        if (conn != null) 
+          conn.close();
+      } catch (Exception e) {
+        throw new RuntimeException( "Could not close the connection");
+      }
+    }
+
+  }
 }
